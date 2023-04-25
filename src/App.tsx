ff2391fa6a9c53bc27bed6c18fe28c91593b7510
Mehow <kaiser.mehow@gmail.com>
2023-04-25 22:24:10 +0100

    gps watch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import possessedSnail from "./possessed_snail.webp";
+import "./App.css";
+import { headingDistanceTo, LatLon, moveTo } from "geolocation-utils";
+
+function SnailChase(props: { location: LatLon; snailLocation: LatLon }) {
+  let { snailLocation, location } = props;
+  var distance;
+  if (snailLocation !== null && location !== null) {
+    distance = headingDistanceTo(snailLocation, location).distance;
+  } else {
+    distance = 0;
+  }
+
+  return (
+    <p>
+      Your location is {location?.lat}, {location?.lon}. The snails location is{" "}
+      {snailLocation?.lat}, {snailLocation?.lon}. Snail is{" "}
+      {Math.round(distance)} meters from you
+    </p>
+  );
+}
 
 function App() {
+  const [location, setLocation] = useState<LatLon | null>(null);
+  const [dealMade, setDealMade] = useState<boolean>(false);
+
+  const [snailLocation, setSnailLocation] = useState<LatLon | null>(null);
+
+  useEffect(() => {
+    const id = navigator.geolocation.watchPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+      return () => {
+        navigator.geolocation.clearWatch(id);
+      };
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!dealMade) {
+      return;
+    }
+    const timer = setInterval(() => {
+      setSnailLocation((snailLocation: LatLon | null) => {
+        if (snailLocation === null) {
+          return location;
+        } else {
+          return moveTo(snailLocation, {
+            heading: 32.1,
+            distance: 1,
+          });
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [dealMade, location]);
+
+  const makeDeal = () => {
+    // make this conditional on the users location already being known
+    setDealMade(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={possessedSnail}
+          alt="Artist's impression of the immortal snail"
+        />
+        {dealMade && snailLocation && location ? (
+          <SnailChase snailLocation={snailLocation} location={location} />
+        ) : (
+          <button onClick={makeDeal}>Make deal with snail</button>
+        )}
       </header>
     </div>
   );
