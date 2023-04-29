077f10ee049dcb442813f216b757a5263d6d2a25
Mehow <kaiser.mehow@gmail.com>
2023-04-29 09:57:31 +0100

    snail time tracking

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,33 @@ import possessedSnail from "./possessed_snail.webp";
 import "./App.css";
 import { headingDistanceTo, LatLon, moveTo } from "geolocation-utils";
 
+// https://hypertextbook.com/facts/1999/AngieYee.shtml
+const SNAIL_METERS_PER_SECOND = 0.013;
+const SNAIL_METERS_PER_MILLISECOND = SNAIL_METERS_PER_SECOND / 1000;
+
+interface SnailInformation {
+  location: LatLon;
+  timestamp: number;
+}
+
+function distanceToSnailTime(meters: number): number {
+  return meters / SNAIL_METERS_PER_MILLISECOND;
+}
+
+function formatTimeTillSnail(milliseconds: number): string {
+  const millisecondsInADay = 1000 * 60 * 60 * 24;
+  const days = Math.floor(milliseconds / millisecondsInADay);
+  milliseconds = milliseconds % millisecondsInADay;
+  const millisecondsInAHour = 1000 * 60 * 60;
+  const hours = Math.floor(milliseconds / millisecondsInAHour);
+  milliseconds = milliseconds % millisecondsInAHour;
+  const millisecondsInAMinute = 1000 * 60;
+  const minutes = Math.floor(milliseconds / millisecondsInAMinute);
+  const millisecondsInASecond = 1000;
+  const seconds = Math.floor(milliseconds / millisecondsInASecond);
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+}
+
 function SnailChase(props: { location: LatLon; snailLocation: LatLon }) {
   let { snailLocation, location } = props;
   var distance;
@@ -15,9 +42,11 @@ function SnailChase(props: { location: LatLon; snailLocation: LatLon }) {
 
   return (
     <p>
-      Your location is {location?.lat}, {location?.lon}. The snails location is{" "}
-      {snailLocation?.lat}, {snailLocation?.lon}. Snail is{" "}
-      {Math.round(distance)} meters from you
+      Your location is {location.lat.toFixed(2)}, {location.lon.toFixed(2)}. The
+      snails location is {snailLocation.lat.toFixed(2)},{" "}
+      {snailLocation.lon.toFixed(2)}. Snail is {Math.round(distance)} meters
+      from you. If you do not move, it will reach you in{" "}
+      {formatTimeTillSnail(distanceToSnailTime(distance))}
     </p>
   );
 }
@@ -26,7 +55,8 @@ function App() {
   const [location, setLocation] = useState<LatLon | null>(null);
   const [dealMade, setDealMade] = useState<boolean>(false);
 
-  const [snailLocation, setSnailLocation] = useState<LatLon | null>(null);
+  const [snailInformation, setSnailInformation] =
+    useState<SnailInformation | null>(null);
 
   useEffect(() => {
     const id = navigator.geolocation.watchPosition((position) => {
@@ -44,15 +74,33 @@ function App() {
     if (!dealMade) {
       return;
     }
+    if (location === null) {
+      return;
+    }
     const timer = setInterval(() => {
-      setSnailLocation((snailLocation: LatLon | null) => {
-        if (snailLocation === null) {
-          return location;
+      setSnailInformation((snailInformation: SnailInformation | null) => {
+        const now = Date.now();
+        if (snailInformation === null) {
+          return {
+            location,
+            timestamp: now,
+          };
         } else {
-          return moveTo(snailLocation, {
-            heading: 32.1,
-            distance: 1,
+          const heading = headingDistanceTo(
+            snailInformation.location,
+            location
+          ).heading;
+          const now = Date.now();
+          const timeSinceLastMove = snailInformation.timestamp - now;
+          const newLocation = moveTo(snailInformation.location, {
+            heading: heading,
+            // todo: logic to make sure the snail doesn't over shoot
+            distance: SNAIL_METERS_PER_MILLISECOND * timeSinceLastMove,
           });
+          return {
+            location: newLocation,
+            timestamp: now,
+          };
         }
       });
     }, 1000);
@@ -74,8 +122,11 @@ function App() {
           src={possessedSnail}
           alt="Artist's impression of the immortal snail"
         />
-        {dealMade && snailLocation && location ? (
-          <SnailChase snailLocation={snailLocation} location={location} />
+        {dealMade && snailInformation && location ? (
+          <SnailChase
+            snailLocation={snailInformation.location}
+            location={location}
+          />
         ) : (
           <button onClick={makeDeal}>Make deal with snail</button>
         )}
