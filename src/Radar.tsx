3b4cd693afc8f7cbb6992b7cb585146cca8c8020
Mehow <kaiser.mehow@gmail.com>
2023-05-06 18:17:19 +0100

    scale slider

--- src/Radar.tsx
+++ src/Radar.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import * as d3 from "d3";
 
 const Axis = ({ domain = [0, 100], range = [0, 500], x = 0, y = 0 }) => {
@@ -46,130 +46,156 @@ interface RadarProperties {
 function Radar(props: RadarProperties) {
   const { snailX, snailY } = props;
 
+  const distance = Math.sqrt(Math.pow(snailX, 2) + Math.pow(snailY, 2));
+
+  const maxAxisValue =
+    distance < 10
+      ? 10
+      : distance < 50
+      ? 50
+      : Math.sqrt(Math.pow(snailX, 2) + Math.pow(snailY, 2)) * 100;
+
+  const [radarRange, setRadarRange] = useState(maxAxisValue);
+
   const rings = Array(10).fill(0);
   const viewBoxHeight = 1000;
   const viewBoxWidth = 1000;
 
-  const distance = Math.sqrt(Math.pow(snailX, 2) + Math.pow(snailY, 2));
-  const maxAxisValue = Math.floor(Math.log10(distance)) * 100;
-  const scaledSnailX = Math.min(1, snailX / maxAxisValue) * 500;
-  const scaledSnailY = Math.min(1, snailY / maxAxisValue) * 500;
+  const scaledSnailX = Math.max(-1, Math.min(1, snailX / radarRange)) * 500;
+  const scaledSnailY = Math.max(-1, Math.min(1, snailY / radarRange)) * 500;
 
   // todo: should we be using D3 to do some of this animating/transforming?
 
   return (
-    <svg viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
-      <defs>
-        <linearGradient
-          id="linear-gradient"
-          gradientUnits="userSpaceOnUse"
-          x1="1000"
-          y1="750"
-          x2="500"
-          y2="750"
-        >
-          <stop offset="90%" stopColor="green" stopOpacity="0%" />
-          <stop offset="100%" stopColor="green" stopOpacity="100%" />
-        </linearGradient>
-        <mask id="cut-off-center">
-          <rect width="100%" height="100%" fill="white" />
-          <circle
-            cx={viewBoxWidth / 2}
-            cy={viewBoxWidth / 2}
-            r={viewBoxWidth / 2}
-            fill="black"
-          />
-        </mask>
-        <clipPath id="clip">
-          <circle r="600" cx="500" cy="500" />
-        </clipPath>
-        <radialGradient id="myGradient">
-          <stop offset="0%" stopColor="green" stopOpacity="100%" />
-          <stop offset="100%" stopColor="white" stopOpacity="0%" />
-        </radialGradient>
-      </defs>
-      <circle
-        cx={viewBoxWidth / 2}
-        cy={viewBoxWidth / 2}
-        r={viewBoxWidth / rings.length}
-        fill="green"
-        stroke="green"
-        strokeWidth={4}
-      />
-      {rings.map((_, i) => (
+    <>
+      <div>
+        <input
+          type="range"
+          min={10}
+          max={1000}
+          onChange={(event) =>
+            setRadarRange(Number.parseInt(event.target.value))
+          }
+        />
+      </div>
+      <svg viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
+        <defs>
+          <linearGradient
+            id="linear-gradient"
+            gradientUnits="userSpaceOnUse"
+            x1="1000"
+            y1="750"
+            x2="500"
+            y2="750"
+          >
+            <stop offset="90%" stopColor="green" stopOpacity="0%" />
+            <stop offset="100%" stopColor="green" stopOpacity="100%" />
+          </linearGradient>
+          <mask id="cut-off-center">
+            <rect width="100%" height="100%" fill="white" />
+            <circle
+              cx={viewBoxWidth / 2}
+              cy={viewBoxWidth / 2}
+              r={viewBoxWidth / 2}
+              fill="black"
+            />
+          </mask>
+          <clipPath id="clip">
+            <circle r="600" cx="500" cy="500" />
+          </clipPath>
+          <radialGradient id="myGradient">
+            <stop offset="0%" stopColor="green" stopOpacity="100%" />
+            <stop offset="100%" stopColor="white" stopOpacity="0%" />
+          </radialGradient>
+        </defs>
         <circle
           cx={viewBoxWidth / 2}
           cy={viewBoxWidth / 2}
-          r={((i + 1) * (viewBoxWidth / rings.length)) / 2}
-          fill="None"
+          r={viewBoxWidth / rings.length}
+          fill="green"
           stroke="green"
           strokeWidth={4}
         />
-      ))}
-      <g>
-        <image
-          x={500 + scaledSnailX - 40}
-          y={500 + scaledSnailY - 40}
-          width="80"
-          height="80"
-          // https://uxwing.com/snail-icon/</g>
-          href="snail-icon.svg"
-        ></image>
-        <circle cx={500 + scaledSnailX} cy={500 + scaledSnailY} r="50">
-          <animate
+        {rings.map((_, i) => (
+          <circle
+            cx={viewBoxWidth / 2}
+            cy={viewBoxWidth / 2}
+            r={((i + 1) * (viewBoxWidth / rings.length)) / 2}
+            fill="None"
+            stroke="green"
+            strokeWidth={4}
+          />
+        ))}
+        <g>
+          <foreignObject
+            x="0"
+            y="0"
+            width="1000"
+            height="1000"
+            clip="url(#clip)"
+          >
+            <div
+              key="makemeunique"
+              style={{
+                width: "100%",
+                height: "100%",
+                background:
+                  "conic-gradient(transparent 150deg, green 180deg, transparent 181deg)",
+              }}
+            ></div>
+          </foreignObject>
+          <line
+            x1={viewBoxWidth / 2}
+            y1={viewBoxWidth / 2}
+            x2={viewBoxWidth / 2}
+            y2={viewBoxWidth}
+            stroke="black"
+          ></line>
+          <animateTransform
+            attributeName="transform"
             attributeType="XML"
-            attributeName="fill"
-            values="url('#myGradient');transparent;"
-            // values="green;transparent;"
-            dur="5s"
+            type="rotate"
+            from={`0 ${viewBoxWidth / 2} ${viewBoxWidth / 2}`}
+            to={`360 ${viewBoxWidth / 2} ${viewBoxWidth / 2}`}
+            dur="10s"
             repeatCount="indefinite"
           />
-        </circle>
-      </g>
-
-      <g>
-        <foreignObject x="0" y="0" width="1000" height="1000" clip="url(#clip)">
-          <div
-            key="makemeunique"
-            style={{
-              width: "100%",
-              height: "100%",
-              background:
-                "conic-gradient(transparent 150deg, green 180deg, transparent 181deg)",
-            }}
-          ></div>
-        </foreignObject>
-        <line
-          x1={viewBoxWidth / 2}
-          y1={viewBoxWidth / 2}
-          x2={viewBoxWidth / 2}
-          y2={viewBoxWidth}
-          stroke="black"
-        ></line>
-        <animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="rotate"
-          from={`0 ${viewBoxWidth / 2} ${viewBoxWidth / 2}`}
-          to={`360 ${viewBoxWidth / 2} ${viewBoxWidth / 2}`}
-          dur="10s"
-          repeatCount="indefinite"
-        />
-      </g>
-      <Axis
-        x={viewBoxWidth / 2}
-        y={viewBoxWidth / 2}
-        domain={[0, maxAxisValue]}
-      ></Axis>
-      <rect
-        x="0"
-        width={viewBoxWidth}
-        y="0"
-        height={viewBoxWidth}
-        fill="green"
-        mask="url(#cut-off-center)"
-      ></rect>
-    </svg>
+        </g>
+        <rect
+          x="0"
+          width={viewBoxWidth}
+          y="0"
+          height={viewBoxWidth}
+          fill="green"
+          mask="url(#cut-off-center)"
+        ></rect>
+        <g>
+          <image
+            x={500 + scaledSnailX - 40}
+            y={500 + scaledSnailY - 40}
+            width="80"
+            height="80"
+            // https://uxwing.com/snail-icon/</g>
+            href="snail-icon.svg"
+          ></image>
+          <circle cx={500 + scaledSnailX} cy={500 + scaledSnailY} r="50">
+            <animate
+              attributeType="XML"
+              attributeName="fill"
+              values="url('#myGradient');transparent;"
+              // values="green;transparent;"
+              dur="5s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </g>
+        <Axis
+          x={viewBoxWidth / 2}
+          y={viewBoxWidth / 2}
+          domain={[0, radarRange]}
+        ></Axis>
+      </svg>
+    </>
   );
 }
 
